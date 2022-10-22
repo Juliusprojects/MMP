cd8bfcb30b7fac644feed4d1dacdfe7030287828
Juliusprojects <67452996+Juliusprojects@users.noreply.github.com>
2022-10-22 21:20:56 +0200

    init2

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private Transform privateTransform = null;
+    private bool jumping;
+    private Rigidbody2D body;
+    private float moveSpeed, dirX;
+
+    private Vector2 normalJump = new Vector2(0, 5);
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+        moveSpeed = 3f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)) {
+            jumping = true;
+            Debug.Log("space");
+        } 
+        dirX = Input.GetAxisRaw("Horizontal") * moveSpeed;
+    }
+
+
+    void FixedUpdate()
+    {
+        if (jumping) {
+           body.AddForce(normalJump, ForceMode2D.Impulse);
+           jumping = false;
+        }
+        body.AddForce(new Vector2(dirX, 0f), ForceMode2D.Force);
+    }
+
+    void OnCollisionEnter2D(Collision2D collisionInfo)
+    {
+        
+    }
+
+    void OnCollisionExit2D(Collision2D collisionInfo)
+    {
+        
+    }
+    
+}
