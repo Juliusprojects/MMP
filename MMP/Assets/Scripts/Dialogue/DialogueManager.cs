cb13136911a4f37d1baf8cf6bfee60d7420e4a9e
Juliusprojects <mail@juliusmeurer.de>
2024-07-16 19:27:47 +0200

    enddialog

--- MMP/Assets/Scripts/Dialogue/DialogueManager.cs
+++ MMP/Assets/Scripts/Dialogue/DialogueManager.cs
@@ -2,8 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using Unity.VisualScripting;
-using UnityEngine.Analytics;
 
 public class DialogueManager : MonoBehaviour
 {
@@ -11,23 +9,24 @@ public class DialogueManager : MonoBehaviour
     public TextMeshProUGUI dialogueText;
     private Queue<string> sentences;
     public static bool isDialogueActive = false;
-
     public static bool isDone = false;
-    // Start is called before the first frame update
+    
+    public GameObject thankYouDialogueCanvas;
+    private bool isThankYouDialogueTriggered = false;
+
     void Start()
     {
         sentences = new Queue<string>();
     }
 
-    // Update is called once per frame
     void Update()
     {
 
     }
 
-    public void StartDialogue (Dialogue dialogue)
+    public void StartDialogue(Dialogue dialogue)
     {
-        if (isDone) return;
+        if (isDone && !isThankYouDialogueTriggered) return;
         isDialogueActive = true;
 
         nameText.text = dialogue.name;
@@ -42,7 +41,7 @@ public class DialogueManager : MonoBehaviour
         DisplayNextSentence();
     }
 
-    public void DisplayNextSentence ()
+    public void DisplayNextSentence()
     {
         if (sentences.Count == 0)
         {
@@ -54,10 +53,38 @@ public class DialogueManager : MonoBehaviour
         dialogueText.text = sentence;
     }
 
-    void EndDialogue () 
+    void EndDialogue()
     {
         isDone = true;
         FindObjectOfType<DialogueTrigger>().dialogueCanvas.SetActive(false);
         Debug.Log("End Dialogue");
+    }
+
+    public void StartThankYouDialogue()
+    {
+        if (isThankYouDialogueTriggered) return;
+        isThankYouDialogueTriggered = true;
+        thankYouDialogueCanvas.SetActive(true);
+        dialogueText.text = "Thank you";
+        StartCoroutine(FadeOutDialogueText());
+    }
+
+    private IEnumerator FadeOutDialogueText()
+    {
+        float fadeDuration = 1.0f;
+        float startAlpha = dialogueText.color.a;
+        float rate = 1.0f / fadeDuration;
+        float progress = 0.0f;
+
+        while (progress < 1.0f)
+        {
+            Color tmpColor = dialogueText.color;
+            dialogueText.color = new Color(tmpColor.r, tmpColor.g, tmpColor.b, Mathf.Lerp(startAlpha, 0, progress));
+            progress += rate * Time.deltaTime;
+            yield return null;
+        }
+
+        dialogueText.color = new Color(dialogueText.color.r, dialogueText.color.g, dialogueText.color.b, 0);
+        thankYouDialogueCanvas.SetActive(false);
     }
 }
